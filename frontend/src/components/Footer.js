472ebd0d9e721e465f962c77af838787e1234d3e
John Patrick Valera <johnpatrickyusoresvalera@gmail.com>
2024-03-05 00:14:13 +0800

    chore: removed unused icon

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,7 +10,6 @@ import Typography from "@mui/material/Typography";
 
 import FacebookIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import TwitterIcon from "@mui/icons-material/X";
 import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 
 const logoStyle = {
